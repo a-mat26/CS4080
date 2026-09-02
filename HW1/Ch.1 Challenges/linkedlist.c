b7d98fca2bb2b352922bd2f2b527f490d2c4820e
Aaron Mathew <aaronsebox@gmail.com>
2026-09-02 11:39:04 -0700

    linked list

--- HW1/Ch.1 Challenges/linkedlist.c
+++ HW1/Ch.1 Challenges/linkedlist.c
@@ -1,1 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Node
+{
+	char *data;
+	struct Node *next;
+	struct Node *prev;
+} Node;
+
+typedef struct
+{
+	Node *head;
+	Node *tail;
+} List;
+
+Node *create_node(const char *str)
+{
+	Node *node = malloc(sizeof(Node));
+	node->data = malloc(strlen(str) + 1);
+	node->prev = NULL;
+	node->next = NULL;
+	return node;
+}
+
+void insert(List *list, const char *str)
+{
+	Node *node = create_node(str);
+	if (list->tail == NULL)
+	{
+		list->head = node;
+		list->tail = node;
+	}
+	else
+	{
+		node->prev;
+		list->tail->next = node;
+		list->tail = node;
+	}
+}
